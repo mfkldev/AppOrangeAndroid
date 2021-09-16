d96728be4858ac9524018882a4348e257daf499e
mfkldev <marcio.souza@zup.com.br>
2021-09-16 18:27:24 -0300

    Layout Screen Login
    Layout da tela de login criado, conforme telas da equipe de design.

---
+++ app/src/main/java/br/com/marciosouza/loginorange/FormLoginActivity.kt
@@ -0,0 +1,11 @@
+package br.com.marciosouza.loginorange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_login)
+    }
+}
